fcfabe12120f833cc1867ffdca30df224acbd5be
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-03-19 19:21:47 +0800

    add dark/light mode toggle

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 'use strict';
-console.log('e');
+
+
+const setupModeToggle = () => {
+    const buttonToggle = document.querySelector('.nav button');
+    const root = document.querySelector(':root');
+    const toggleIcon = document.querySelector('.nav button i')
+    buttonToggle.addEventListener('click', () => {
+        root.classList.toggle('toggle');
+        if (toggleIcon.classList.contains('fa-moon')) {
+            toggleIcon.classList.remove('fa-moon');
+            toggleIcon.classList.add('fa-sun');
+        } else {
+            toggleIcon.classList.remove('fa-sun');
+            toggleIcon.classList.add('fa-moon');
+        }
+    })
+}
+
+
+setupModeToggle();
+
