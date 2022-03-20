87b0d650b0c7bc15f0157b12862e922226b5944a
James Esguerra <james.a.esguerra@obf.ateneo.edu>
2022-03-20 11:13:41 +0800

    fix dark/light mode toggle

--- script.js
+++ script.js
@@ -13,26 +13,9 @@ const setupTheme = () => {
 const setupModeToggle = () => {
     const buttonToggle = document.querySelector('.nav button');
     const root = document.querySelector(':root');
-    const toggleIcon = document.querySelector('.nav button i')
-    const paths = document.querySelectorAll('.nav__icon svg path');
-    const circle = document.querySelector('circle');
     buttonToggle.addEventListener('click', () => {
         root.classList.toggle('toggle');
-        if (toggleIcon.classList.contains('fa-moon')) {
-            toggleIcon.classList.remove('fa-moon');
-            toggleIcon.classList.add('fa-sun');
-            paths.forEach(path => {
-                path.setAttribute('fill', 'white');
-            })
-            circle.setAttribute('fill', 'white');
-        } else {
-            toggleIcon.classList.remove('fa-sun');
-            toggleIcon.classList.add('fa-moon');
-            paths.forEach(path => {
-                path.setAttribute('fill', 'black');
-            })
-            circle.setAttribute('fill', 'black');
-        }
+        
     })
 }
 
